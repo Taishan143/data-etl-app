719573e6a9099c1b4541121ffcb0ec5a7085d4fb
Taishan314 <taishan.rowe@essencemediacom.com>
2025-05-23 14:56:58 +0100

    adding final bits

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# set up virtual environment
+
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+
+python3 -m pip install -r requirements.txt
+
+# Run the app
+
 python3 -m data_etl_app/main.py kaggle-config.yaml
