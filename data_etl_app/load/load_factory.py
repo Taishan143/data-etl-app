ea301d89a0bc25a9a84338d76b5ea848e1955930
Taishan314 <taishan.rowe@essencemediacom.com>
2025-05-28 08:33:14 +0100

    added sqlite3 loader

--- data_etl_app/load/load_factory.py
+++ data_etl_app/load/load_factory.py
@@ -1,14 +1,13 @@
 from typing import Union, TypeAlias
 from data_etl_app.load.sources.mysql_loader import MySQLLoader
+from data_etl_app.load.sources.sqlite_loader import SQLliteLoader
 
 Loader: TypeAlias = Union[MySQLLoader]
 
 
 def get_loader_instance(database_name: str) -> Loader:
 
-    api_mapping: dict[str, Loader] = {
-        "mysql": MySQLLoader,
-    }
+    api_mapping: dict[str, Loader] = {"mysql": MySQLLoader, "sqlite": SQLliteLoader}
     try:
         return api_mapping[database_name]
     except KeyError:
